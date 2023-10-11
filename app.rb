faf824304f5d5d9529aa209463157225026a817f
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-11 15:30:52 +0100

    Built Node class

--- app.rb
+++ app.rb
@@ -1,45 +1,17 @@
-$substrings_list = []
-
-def substrings (word, dictionary)
-    dictionary = dictionary.map {|str| str.downcase}
-    word = word.downcase
-    $size = word.length
-    hash = {}
-
-    find_subs(word)
-
-    $substrings_list.each do |sub_string|
-        dictionary.each do |dict_word|
-            if sub_string == dict_word
-                if hash[sub_string] != nil
-                    hash[sub_string] += 1
-                else
-                    hash[sub_string] = 1
-                end
-            end
-        end
-    end
-
-    return hash
+class LinkedList
 end
 
-def find_subs (word, index=0, target=0)
-    if word.empty?
-        return
-    end
-    
-    while target < word.length
-        $substrings_list.push(word[index..target]) 
-        target+=1
+class Node
+    def initialize
+        @value = nil
+        @next_node = nil
     end
 
-    if index<=$size
-        word = word[1..-1]
-        find_subs(word, index=0, target=0)
-    else
-        return
+    def value 
+        @value
+    end
+
+    def next_node
+        @next_node
     end
 end
-
-# list = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# substrings("Howdy partner, sit down! How's it going?", list)
