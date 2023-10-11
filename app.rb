6c5c8c974c5393d34ef62fcbaf6e28f3096651d4
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-11 17:05:03 +0100

    Assignment completed

--- app.rb
+++ app.rb
@@ -43,7 +43,68 @@ class LinkedList
         @tail
     end
 
+    def at(index)
+        if index == 1
+            return @head
+        end
+        
+        node = @head
+        while index > 1
+           node = node.next_node
+           index-=1
+        end 
+        return node
+    end
 
+    def pop
+        i = 1
+        until self.at(i) == @tail
+            i+=1
+        end
+        self.at(i-1).next_node = nil
+        @tail = self.at(i-1)
+    end
+
+    def contains?(value)
+        if @tail.value == value
+            return true
+        end
+
+        i = 1
+        until self.at(i) == @tail
+            if self.at(i).value == value
+                return true
+            end
+            i+=1
+        end
+        return false
+    end
+    
+    def find(value) 
+        unless contains?(value)
+            return nil
+        end
+        
+        i=1
+        until self.at(i) == @tail
+            if self.at(i).value == value
+                return i
+            end
+            i+=1
+        end
+        return i
+    end
+
+    def to_s
+        node = @head
+        
+        until node.next_node == nil
+            print "( #{node.value} ) -> "
+            node = node.next_node
+        end
+        print "( #{self.tail.value} ) -> nil"
+        puts
+    end
 
 end
 
@@ -65,15 +126,17 @@ class Node
     end
 end
 
-l = LinkedList.new
-l.append(4)
-l.append(5)
-l.append(6)
-l.prepend(9)
 
+# l = LinkedList.new
+# l.append(4)
+# l.append(5)
+# l.append(6)
+# l.prepend(9)
+# l.append(10)
+# l.prepend(7)
 
-# p l.head.value
-# p l.head.next_node
-p l.head.next_node.value
-p l.tail
-p l.size
+# p l.head
+# p l.tail
+# p l.size
+# p l.find(10)
+# l.to_s
